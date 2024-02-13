d8da39659f2b48c6086c6f87341c4f56deb9de4e
Tolgay Altiner <tolgay.altiner@web.de>
2024-02-14 00:58:25 +0100

    custom Validations für datefields und location hinzugefügt

--- src/app/add-travel/add-travel.component.ts
+++ src/app/add-travel/add-travel.component.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-add-travel',
   templateUrl: './add-travel.component.html',
-  styleUrl: './add-travel.component.css'
+  styleUrls: ['./add-travel.component.css']
 })
 export class AddTravelComponent implements OnInit {
   travelForm: FormGroup;
@@ -15,10 +15,10 @@ export class AddTravelComponent implements OnInit {
 
   initializeForm(){
     this.travelForm = new FormGroup({
-      'startDate': new FormControl(null, Validators.required),
+      'startDate': new FormControl(null, [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/)]),
       'endDate': new FormControl(null, Validators.required),
-      'country': new FormControl(null, Validators.required),
-      'city': new FormControl(null, Validators.required),
+      'country': new FormControl(null, [Validators.required, this.validateLocation.bind(this)]),
+      'city': new FormControl(null, [Validators.required, this.validateLocation.bind(this)]),
       'activities': new FormControl(null, Validators.required),
       'comment': new FormControl(null, Validators.required),
       'rating': new FormGroup({
@@ -29,5 +29,42 @@ export class AddTravelComponent implements OnInit {
 
   onSubmit(){
     console.log(this.travelForm);
+  }
+
+  validateLocation(control: FormControl): {[s:string]: boolean} | null{
+   if(control.value){
+    if(control.value.length > 20){
+      return {'maxLengthError': true};
+    }else if(!this.isPatternCorrect('locationPattern', control.value)){
+      return {'onlyLettersError': true};
+    }else {
+      return null;
+    }
+   }
+    return null; 
+  }
+
+
+
+  validateDate(control: FormControl): {[s:string]: boolean} | null{
+    if(control.value && !this.isPatternCorrect('datePattern', control.value)){
+      console.log('invalid Date')
+      return {'datePatternError': true};
+    }else {
+      return null;
+    }
+  }
+
+  isPatternCorrect(toBeChecked: string, inputString: any): boolean{
+   let pattern: RegExp;
+   switch(toBeChecked){
+    case 'locationPattern':
+     pattern = /^[a-zA-Z]+$/;
+     break;
+    case 'datePattern': 
+      pattern = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
+    break;  
+    }
+    return pattern.test(inputString);
   }
 }
